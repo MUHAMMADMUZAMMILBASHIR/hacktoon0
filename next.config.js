9aba83ccb8b3768e1c99168615c707826fd531ba
MUHAMMADMUZAMMILBASHIR <126901892+MUHAMMADMUZAMMILBASHIR@users.noreply.github.com>
2023-09-06 12:52:49 +0500

    Add files via upload
    mz

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },images: {
+    domains: ['localhost', "res.cloudinary.com"],
+  }
+};
+
+module.exports = nextConfig;
